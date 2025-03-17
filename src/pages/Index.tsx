28cf4889e56d15e3944a280b1d367f6f2fcf6a10
0x1A4 <nshcore@protonmail.com>
2025-03-17 09:39:37 -0500

    Update Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -3,7 +3,7 @@ import React, { useState } from 'react';
 import Navbar from '../components/Navbar';
 import Footer from '../components/Footer';
 import HeroSection from '../components/HeroSection';
-import Services from '../components/Services';
+import Services from '../components/ServicesSection';
 import CTASection from '../components/CTASection';
 
 const Index: React.FC = () => {
@@ -14,6 +14,7 @@ const Index: React.FC = () => {
       <main className="flex-grow">
         <HeroSection />
         <Services />
+        <ServicesSection />
         <CTASection />
       </main>
       
