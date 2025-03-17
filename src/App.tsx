48c9cf57abcda92670a1ebb1395d83e992b2c316
0x1A4 <nshcore@protonmail.com>
2025-03-17 10:06:16 -0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,6 @@ import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
-import Services from "./pages/Services";
 import About from "./pages/About";
 import Contact from "./pages/Contact";
 import HireUs from "./pages/HireUs";
